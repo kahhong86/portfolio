7164726f8ed7eea35270be3d04c5192052e98626
Cheong Kah Hong <Prince@Cheongs-MacBook-Pro.local>
2022-01-27 16:58:11 +0800

    Instagram json and new embed method

--- pages/comic.tsx
+++ pages/comic.tsx
@@ -1,5 +1,6 @@
 import Layout from "../src/components/layout";
 import { FunctionComponent } from "react";
+import InstagramEmbed from "react-instagram-embed"
 
 interface ComicProps{}
 
@@ -7,6 +8,20 @@ const Comic:FunctionComponent<ComicProps> = () => {
     return(
         <Layout title="Comic">
             <h1>Comic</h1>
+            <InstagramEmbed
+            url='https://www.instagram.com/kahhong86/'
+            clientAccessToken='899020530779900|41230abd83f515d346f0883d5a5e2e86'
+            maxWidth={320}
+            hideCaption={false}
+            containerTagName='div'
+            protocol=''
+            injectScript
+            onLoading={() => {}}
+            onSuccess={() => {}}
+            onAfterRender={() => {}}
+            onFailure={() => {}}
+            />
+            a
         </Layout>
     )
 }
